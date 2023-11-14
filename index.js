34f1c0d3c99f9e5c62d72c7a09c3eed8e2bd6f74
Juan Quino <quinojuan@gmail.com>
2023-11-14 14:59:04 -0300

    finished?

--- index.js
+++ index.js
@@ -13,4 +13,28 @@ const addTask = () => {
     li.appendChild(span);
   }
   inputBox.value = "";
+  saveData();
 };
+
+listContainer.addEventListener(
+  "click",
+  (e) => {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+const saveData = () => {
+  localStorage.setItem("data", listContainer.innerHTML);
+};
+
+const showTask = () => {
+  listContainer.innerHTML = localStorage.getItem("data");
+};
+showTask();
